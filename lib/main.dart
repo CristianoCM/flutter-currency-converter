77f3188f5d1163c76d6943776eecad7c1659cc93
Cristiano <cristiano@blanko.be>
2020-03-05 11:40:07 -0300

    functions

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ Future<Map> getData() async {
 Widget buildTextField(String label, String prefix, TextEditingController con, Function f) {
   return TextField(
     controller: con,
-    keyboardType: TextInputType.number,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
     decoration: InputDecoration(
         labelText: label,
         labelStyle: TextStyle(
@@ -54,34 +54,68 @@ class _HomeState extends State<Home> {
   final dollarController = TextEditingController();
   final euroController = TextEditingController();
 
+  double real;
   double dollar;
   double euro;
 
+  void _resetStates() {
+    realController.text = "";
+    dollarController.text = "";
+    euroController.text = "";
+  }
+
   void _realChanged(String text) {
-    
+    if (text.isEmpty) {
+      _resetStates();
+    }
+
+    double realNow = double.parse(text);
+
+    setState(() {
+      dollarController.text = (realNow / dollar).toStringAsFixed(2);
+      euroController.text = (realNow / euro).toStringAsFixed(2);
+    });
   }
 
   void _dollarChanged(String text) {
+    if (text.isEmpty) {
+      _resetStates();
+    }
+
+    double dollarNow = double.parse(text);
     
+    setState(() {
+      realController.text = (dollarNow * dollar).toStringAsFixed(2);
+      euroController.text = (dollarNow * dollar / euro).toStringAsFixed(2);
+    });
   }
 
   void _euroChanged(String text) {
+    if (text.isEmpty) {
+      _resetStates();
+    }
+
+    double euroNow = double.parse(text);
     
+    setState(() {
+      realController.text = (euroNow * euro).toStringAsFixed(2);
+      dollarController.text = (euroNow * euro / dollar).toStringAsFixed(2);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text("Currency Converter"),
-            centerTitle: true,
-            backgroundColor: Colors.amber),
+          title: Text("Currency Converter"),
+          centerTitle: true,
+          backgroundColor: Colors.amber
+        ),
         body: FutureBuilder<Map>(
           future: getData(),
           builder: (context, snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.none:
-              case ConnectionState.waiting:
                 return Center(
                   child: Text(
                     "Loading data...",
@@ -123,7 +157,7 @@ class _HomeState extends State<Home> {
                           Divider(),
                           buildTextField("Dollar", "US\$ ", dollarController, _dollarChanged),
                           Divider(),
-                          buildTextField("Dollar", "€ ", euroController, _euroChanged),
+                          buildTextField("Euro", "€ ", euroController, _euroChanged),
                         ],
                       )
                   );
